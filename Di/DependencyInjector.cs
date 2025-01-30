fb20180e1799bed145f165ec2a4a0f7f1564ff27
Enigma010 <jjodarsk@yahoo.com>
2025-01-29 22:51:41 -0600

    Initial commit

---
+++ Di/DependencyInjector.cs
@@ -0,0 +1,20 @@
+﻿using DotNetApiDb;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DotNetApiMongoDb.Di
+{
+    [ExcludeFromCodeCoverage(Justification = "Core infrastructure, unit tests would be in a different project")]
+    public static class DependencyInjector
+    {
+        /// <summary>
+        /// Does dependency injection for MongoDB
+        /// </summary>
+        /// <param name="builder">The host application builder</param>
+        public static void AddMongoDbDependencies(this IHostApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<IDbClient, MongoDbClient>();
+        }
+    }
+}
